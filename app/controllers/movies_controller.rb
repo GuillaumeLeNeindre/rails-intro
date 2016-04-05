797df84825746d8da0a857c7b70674b4ba3ae890
Guillaume Le Neindre <guillaume.le.n@gmail.com>
2016-04-05 20:04:39 +0000

    hw 2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,19 +7,46 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort = params[:sort]
+    @all_ratings = Movie.all_ratings
+    
+    
+     
+    @selected_ratings = params[:ratings]
+    if @selected_ratings == nil
+      @selected_ratings = {}
+      @all_ratings.each do |current_rating|
+        @selected_ratings[current_rating] = "1"
+      end
+      @selected_ratings
+    end
+    if @selected_ratings != nil && !@selected_ratings.empty?
+      index = 0
+      sql_query = ""
+      @selected_ratings.each_key do |selected_rating|
+        index = index + 1
+        sql_query = sql_query + %Q<rating = "#{selected_rating}" OR >
+      end
+      sql_query.gsub!(/ OR $/, "")
+      @movies = Movie.where(sql_query)
+    else
+      @movies = Movie
+    end
+    
     @hilite_release_date = false
     @hilite_title = false
+    
+    sort = params[:sort]
+    
     if sort != nil
       if sort == "title"
-        @movies = Movie.order(:title)
+        @movies = @movies.order(:title)
         @hilite_title = true
       elsif sort == "release_date"
-        @movies = Movie.order(:release_date)
+        @movies = @movies.order(:release_date)
         @hilite_release_date = true
       end 
     else
-      @movies = Movie.all
+      @movies = @movies.all
     end
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
 # Connect the attributes to the DB
   attr_accessible :title, :rating, :description, :release_date
+  
+  def self.all_ratings
+      query = select('DISTINCT rating').map(&:rating)
+  end
 end

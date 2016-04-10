3494fa175d75f93b1c99bc239e43276dcb981b46
Guillaume Le Neindre <guillaume.le.n@gmail.com>
2016-04-10 22:01:27 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,23 +7,27 @@ class MoviesController < ApplicationController
   end
 
   def index
+    do_redirect = false
     @all_ratings = Movie.all_ratings
     
-    
-     
-    @selected_ratings = params[:ratings]
-    if @selected_ratings == nil
+    if params[:ratings] != nil
+      @selected_ratings = params[:ratings]
+      session[:ratings] = @selected_ratings
+    elsif session[:ratings] != nil
+      @selected_ratings = session[:ratings]
+      do_redirect = true
+    else
       @selected_ratings = {}
       @all_ratings.each do |current_rating|
         @selected_ratings[current_rating] = "1"
       end
-      @selected_ratings
+      session[:ratings] = @selected_ratings
+      do_redirect = true
     end
+    
     if @selected_ratings != nil && !@selected_ratings.empty?
-      index = 0
       sql_query = ""
       @selected_ratings.each_key do |selected_rating|
-        index = index + 1
         sql_query = sql_query + %Q<rating = "#{selected_rating}" OR >
       end
       sql_query.gsub!(/ OR $/, "")
@@ -35,7 +39,13 @@ class MoviesController < ApplicationController
     @hilite_release_date = false
     @hilite_title = false
     
-    sort = params[:sort]
+    if params[:sort] != nil
+      sort = params[:sort]
+      session[:sort] = sort
+    elsif session[:sort] != nil
+      sort = session[:sort]
+      do_redirect = true
+    end
     
     if sort != nil
       if sort == "title"
@@ -48,6 +58,12 @@ class MoviesController < ApplicationController
     else
       @movies = @movies.all
     end
+    if do_redirect
+      flash.keep
+      debugger
+      redirect_to :controler => 'movies', :action => 'index', :ratings => session[:ratings], :sort => session[:sort]
+    end
+    
   end
 
   def new
